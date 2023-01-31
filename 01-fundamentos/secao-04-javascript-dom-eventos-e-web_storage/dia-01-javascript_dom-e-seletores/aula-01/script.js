33aa0f2bd95071452bdd63c8a2af8feddfe5df84
Pedro Rener <renerpedro@gmail.com>
2023-01-31 15:59:01 -0300

    Conteúdos da aula

---
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-01-javascript_dom-e-seletores/aula-01/script.js
@@ -0,0 +1,9 @@
+document.getElementById("page-title").innerText = 'Perdido em Marte'
+document.getElementById("paragraph").innerText = 'Data de lançamento: 2015'
+document.getElementById("subtitle").innerText = 'Enredo:'
+
+document.getElementById("second-paragraph").innerText = 'A tripulação da missão espacial Ares 3 recolhem material em Acidalia Planitia no planeta Marte em Sol 18. Devido a uma forte tempestade são obrigados a abortar a missão. Na evacuação, um deles, Mark Watney, é atingido por destroços. Os outros acreditam que ele morreu e partem. A NASA informa ao público o ocorrido e seu funeral simbólico é realizado. Porém, Watney sobreviveu: um pedaço de antena e sangue seco tamparam o furo em seu traje. Medica-se e passa registrar seu cotidiano em vídeo.'
+
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
---
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-01-javascript_dom-e-seletores/aula-02/script.js
@@ -0,0 +1,4 @@
+let paragraph = document.getElementsByClassName('paragraph')[0];
+paragraph.style.color = 'red';
+
+document.getElementsByTagName('h4')[0].innerText = 'teste';

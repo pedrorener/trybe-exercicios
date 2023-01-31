27efc6c4d9dd39ee0cdcd40af19e508ee1689f50
Pedro Rener <renerpedro@gmail.com>
2023-01-31 19:41:11 -0300

    Exercícios sobre seletores

---
+++ 01-fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-01-javascript_dom-e-seletores/exercicio-seletores/script.js
@@ -0,0 +1,21 @@
+//Meu body
+document.body.style.backgroundColor = "#f8f8f8";
+
+// Meu Cabeçalho
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "#019450";
+header.style.borderTop = "16px solid black";
+
+// Meu Conteúdo Principal
+document.querySelectorAll("section")[0].style.backgroundColor = "#fa8072";
+document.querySelectorAll("section")[1].style.backgroundColor = "#f0c76f";
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "#6a5acd";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "#6a5acd";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+
+// Meu Rodapé
+const footer = document.getElementById("footer-container");
+footer.style.backgroundColor = "#00251d";
+footer.style.borderBottom = "16px solid black";
+
